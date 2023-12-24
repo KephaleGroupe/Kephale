4c6b3f5ac24a2f15489d248893ec77841a3d4f7f
Paul Koné <paulkone@MacBook-Pro-de-Paul.local>
2023-12-24 01:34:38 +0000

    first commit

--- kephale_bdd/kephale_bdd.php
+++ kephale_bdd/kephale_bdd.php
@@ -1,5 +1,6 @@
 <?php
+$db = new PDO('mysql:host=sql213.infinityfree.com; dbname=if0_35242965_kephale', 'if0_35242965' , 'D1A4wD9OO4vQt' ); 
 
-$db = new PDO('mysql:host=localhost; dbname=kephale', 'root' , 'root' ); 
+//$db = new PDO('mysql:host=localhost; dbname=kephale', 'root' , 'root' ); 
 
 ?>
